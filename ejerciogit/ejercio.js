b98d18216be5ab733fb0f47464df90e180387812
Gabriela Altamirano <gabrielanieto289@gmail.com>
2023-12-17 15:07:11 -0500

    el boton cambia la imagen

--- ejerciogit/ejercio.js
+++ ejerciogit/ejercio.js
@@ -1,3 +1,6 @@
 function saludar(){
+    let cmpImagenSaludo;
+    cmpImagenSaludo=document.getElementById("imgSaludo");
+    cmpImagenSaludo.src="./Imagenes/ganar.gif"
     console.log("ingresa a saludar");
 }
