b49e87d311e4b015d4bd48538c8325df55348152
Gabriela Altamirano <gabrielanieto289@gmail.com>
2023-12-23 15:08:38 -0500

    Funcion mostrarTexto implementada

--- utilitarios/saludo.js
+++ utilitarios/saludo.js
@@ -3,6 +3,8 @@ function saludar(){
     let apellido=recuperaraTexto("txtApellido");
     let edad =recuperarInt("txtEdad");
     let estatura =recuperarFloat("txtEstatura");
+    let mensjeBienvenido ="Bienvenido " +nombre+" "+apellido;
+    mostrarTexto("lblResultado",mensjeBienvenido);
 }
 function recuperaraTexto(idComponente){
 let componente;
@@ -20,4 +22,9 @@ function recuperarFloat(idComponente){
     let valorCaja=recuperaraTexto(idComponente);
     let valorFlotante=parseFloat(valorCaja);
     return valorFlotante;
+}
+function mostrarTexto(idComponente,mensaje){
+    let componente;
+    componente=document.getElementById(idComponente);
+    componente.innerText=mensaje;
 }
