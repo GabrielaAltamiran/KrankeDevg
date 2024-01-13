1739476e4f36162f800b9643cc70b76db43fcea5
Gabriela Altamirano <gabrielanieto289@gmail.com>
2024-01-12 20:24:22 -0500

    Se crea un funcion generarRuta en el archivo serviciJuego.js

--- JUEGO/servicioJuego.js
+++ JUEGO/servicioJuego.js
@@ -11,3 +11,20 @@ function generarElementos(){
         return "TIJERAS";
     }
 }
+function determinarGanador(eleccionJugador1,eleccionJugador2){
+    if (eleccionJugador1 === eleccionJugador2) {
+        return 0; // Empate
+    } else if (
+        (eleccionJugador1 === "piedra" && eleccionJugador2 === "tijera") ||
+        (eleccionJugador1 === "papel" && eleccionJugador2 === "piedra") ||
+        (eleccionJugador1 === "tijera" && eleccionJugador2 === "papel")
+    ) {
+        return 1; // Jugador 1 gana
+    } else {
+        return 2; // Jugador 2 gana
+    }
+}
+function generarRuta(nombre){
+    let rutaImagen = "./imagenes/" + nombre + ".png";
+    return rutaImagen;
+}
